af5debf72cba44e27bd58ffa510475b764b0d1c4
jaronaearle <jaronaearle@gmail.com>
2020-08-24 13:37:10 -0600

    chapter one code

---
+++ 01-gettingIntoGo/01-2-named-returns/returns.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func Names() (first string, second string) {
+	first = "Foo"
+	second = "Bar"
+	return
+}
+
+func main() {
+	n1, n2 := Names()
+	fmt.Println(n1, n2)
+}
